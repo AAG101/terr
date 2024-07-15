4cc17d507ca83c6db66c306730ce605924860a27
shuga <shuga@shugas-MBP.lan>
2024-07-15 18:53:44 -0500

    config.tf

---
+++ config.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+	resource "aws_instance" "deployment-22" {
+	  ami           = "ami-0a699202e5027c10d"
+	  instance_type = "t3.micro"
+	  tags = {
+    Name = "HelloWorld22"
+      }
+    }
